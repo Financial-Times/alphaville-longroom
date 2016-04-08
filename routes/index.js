b2ad138d6928f59bfab5e33e36dde4b8a072a57d
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-04-08 12:51:59 +0100

    Add gtg route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alphaville Longroom' });
+router.get('/', (req, res) => {
+	res.render('index', { title: 'Alphaville Longroom' });
+});
+router.get('/__gtg', (req, res) => {
+	res.sendStatus(200);
 });
 
 module.exports = router;
