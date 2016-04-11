6305cf15477e6876cdfc0714fd7bdece74205cb8
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-04-11 12:02:19 +0100

    Fix for longroom route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-	res.render('index', { title: 'Alphaville Longroom' });
-});
 router.get('/longroom', (req, res) => {
-	res.sendStatus(200);
+	res.render('index', { title: 'Alphaville Longroom' });
 });
 router.get('/__gtg', (req, res) => {
 	res.sendStatus(200);
