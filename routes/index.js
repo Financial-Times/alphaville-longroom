4545fe0a4827543c16c5beab01938f53d2ffed89
Roland Vachter <roland.vachter@iquestgroup.com>
2016-04-26 14:41:03 +0300

    Commented out code removed.

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,6 @@ const router = express.Router();
 const renderPage = require('alphaville-page-render');
 const headerConfig = require('alphaville-header-config');
 
-/*router.get('/', (req, res) => {
-	res.render('index', {
-		title: 'Alphaville Longroom',
-
-		assetsBasePath: '/assets/longroom',
-		basePath: '/longroom',
-
-		isTest: process.env.ENVIRONMENT === 'test' ? true : false,
-		isProd: process.env.ENVIRONMENT === 'prod' ? true : false,
-
-		headerConfig: headerConfig,
-		partials: {
-			header: '../bower_components/alphaville-header/main.hjs'
-		}
-	});
-});
-*/
-
 router.get('/', (req, res) => {
 	renderPage(res, 'index', 'longroom', {
 		title: 'Alphaville Longroom',
