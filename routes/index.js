9449798c4cfb73df95a028fff0afbf92bcc10bab
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-05-04 11:00:31 +0300

    Update versions for alphaville-express and auth middleware

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,9 @@ const express = require('express');
 const router = new express.Router();
 const fs = require('fs');
 const path = require('path');
+const auth = require('alphaville-auth-middleware');
 
-const externalPartials = {
-	barrier: fs.readFileSync(path.join(__dirname, '../bower_components/alphaville-barrier/main.handlebars'), 'utf-8')
-};
-
-const authConfig = {
-	checkHeader: process.env['AUTH_HEADER'],
-	access: process.env['AUTH_HEADER_VALUE']
-};
-
-router.use('/', (req, res, next) => {
-	res.set('Vary', authConfig.checkHeader);
-	if(req.get(authConfig.checkHeader) === authConfig.access) {
-		return next();
-	}
-	return res.render('barrier', {
-		title: 'FT Alphaville',
-		barrierModel: req.barrierModel,
-		partials: {
-			barrier: externalPartials.barrier
-		}
-	});
-});
+router.use('/', auth());
 
 router.get('/', (req, res) => {
 	res.render('index', {
