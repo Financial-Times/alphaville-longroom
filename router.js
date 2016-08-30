85328cecb45bafccf7ebdadd468bbd57400d8470
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-08-30 12:06:39 +0300

    Refactor views to reflect the new routes

--- router.js
+++ router.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const router = new express.Router();
 const auth = require('alphaville-auth-middleware');
+const userMiddleware = require('./lib/middlewares/user');
 
 router.use('/', auth());
 
 router.use('/', require('./routes/index'));
 router.use('/', require('./routes/discussionsRouter'));
 router.use('/content', require('./routes/contentRouter'));
-router.use('/join', require('./routes/membershipRouter'));
+router.use('/user', userMiddleware, require('./routes/userRouter'));
 
 module.exports = router;
