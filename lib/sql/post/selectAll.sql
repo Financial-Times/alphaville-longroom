c1ac6c4d34f36bf0196115a3ecc883b675ceda76
Roland Vachter <roland.vachter@iquestgroup.com>
2016-12-15 15:45:43 +0200

    Fix relations in the post fetching query.

--- lib/sql/post/selectAll.sql
+++ lib/sql/post/selectAll.sql
@@ -1,25 +1,25 @@
 SELECT
-	p.*,
-	t.name tag_name,
-	t.id tag_id,
-	ttp.index tag_index,
-	f.name file_name,
-	f.size file_size,
-	f.ext file_ext,
-	f.source file_source
+    p.*,
+    t.name tag_name,
+    t.id tag_id,
+    ttp.index tag_index,
+    f.name file_name,
+    f.size file_size,
+    f.ext file_ext,
+    f.source file_source
 FROM
-	posts p
-	LEFT JOIN tags_to_posts ttp ON ttp.post_id = p.id
-	JOIN tags t ON t.id = ttp.tag_id
-	LEFT JOIN files f ON f.post_id = p.id
+    posts p
+    LEFT JOIN tags_to_posts ttp ON ttp.post_id = p.id
+    JOIN tags t ON t.id = ttp.tag_id
+    LEFT JOIN files f ON f.post_id = p.id
 WHERE
-	p.id IN (
-		SELECT id
-		FROM posts
-		WHERE
-			p.published != false
-		ORDER BY p.published_at DESC
-		OFFSET ${offset}
-		LIMIT ${limit}
-	)
+    p.id IN (
+        SELECT pi.id
+        FROM posts pi
+        WHERE
+            pi.published != false
+        ORDER BY pi.published_at DESC
+        OFFSET ${offset}
+        LIMIT ${limit}
+    )
 ORDER BY p.published_at DESC
