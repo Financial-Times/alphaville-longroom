3841bde346d31d15c68c77c4f8fd255e8d7eb7f7
Roland Vachter <roland.vachter@iquestgroup.com>
2017-01-04 15:11:29 +0200

    Action buttons added. File icons on post page re-arranged and made smaller.

--- routes/contentRouter.js
+++ routes/contentRouter.js
@@ -6,11 +6,12 @@ const controllers = {
 	create: require('../lib/controllers/create'),
 	view: require('../lib/controllers/view')
 };
+const userMiddleware = require('../lib/middlewares/user');
 
 router.get('/write-post', controllers.create.getWriteAPostForm);
 router.get('/upload-document', controllers.create.getUploadDocumentForm);
 router.post('/create', controllers.create.post);
 
-router.route('/:id').get(controllers.view);
+router.route('/:id').get(userMiddleware, controllers.view);
 
 module.exports = router;
