b9f922c26b236fb4eea38c838353a70eb7ba4130
laurent-muchacho <laurent.muchacho@ft.com>
2017-01-09 15:33:27 +0000

    update summary in edit my posts

---
+++ lib/sql/user/selectSummary.sql
@@ -0,0 +1,3 @@
+SELECT u.summary
+FROM users u
+WHERE u.user_id = ${user_id}
--- routes/mypostsRouter.js
+++ routes/mypostsRouter.js
@@ -5,11 +5,14 @@ console.log('>>> mypostsRouter: ');
 const express = require('express');
 const router = new express.Router();
 const mypostsCtrl = require('../lib/controllers/myposts');
+const updateUserSummaryCtrl = require('../lib/controllers/updateUserSummary');
 
 const userMiddleware = require('../lib/middlewares/user');
 
 router.route('/')
 	.get(userMiddleware, mypostsCtrl);
 
+router.route('/update')
+	.get(userMiddleware, updateUserSummaryCtrl);
 
 module.exports = router;
