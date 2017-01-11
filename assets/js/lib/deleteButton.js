c00fb9ac26aaf5a903017922094b17415410983d
Roland Vachter <roland.vachter@iquestgroup.com>
2017-01-11 13:12:14 +0200

    Fix delete button for Firefox.

--- assets/js/lib/deleteButton.js
+++ assets/js/lib/deleteButton.js
@@ -5,7 +5,7 @@ document.addEventListener('o.DOMContentLoaded', () => {
 	const deleteDelegate = new Delegate(document.body);
 
 	deleteDelegate.on('click', '.lr-action--delete', (evt) => {
-		const postId = evt.srcElement.getAttribute('data-post-id');
+		const postId = evt.target.getAttribute('data-post-id');
 
 		new ConfirmOverlay('Delete post', 'Are you sure you want to delete this post?').then(decision => {
 			if (decision) {
