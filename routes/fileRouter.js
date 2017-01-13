7b100061dc7b63a76b1b11c5a2d207aea4445889
Roland Vachter <roland.vachter@iquestgroup.com>
2017-01-13 18:41:05 +0200

    Fix file upload on main browsers. Add route for wysiwyg attached documents.

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -6,6 +6,7 @@ const fileCtrl = require('../lib/controllers/file');
 
 router.get('/sign', fileCtrl.sign);
 router.post('/delete', fileCtrl.delete);
+router.route('/attachment/:filePath').get(fileCtrl.oldAttachments);
 router.route('/:fileName').get(fileCtrl.download);
 
 module.exports = router;
