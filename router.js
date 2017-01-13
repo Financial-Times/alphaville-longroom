d1b25c23549469d46978607bd775716e08ec629d
Roland Vachter <roland.vachter@iquestgroup.com>
2017-01-13 13:15:58 +0200

    Check if the user has pseudonym or not, and force setting it before being able to access the site.

--- router.js
+++ router.js
@@ -4,17 +4,19 @@ const express = require('express');
 const router = new express.Router();
 const auth = require('alphaville-auth-middleware');
 const userMiddleware = require('./lib/middlewares/user');
+const checkPseudonymMiddleware = require('./lib/middlewares/checkPseudonym');
+
 
 router.use('/', auth(), userMiddleware, require('./routes/index'));
-router.use('/home', auth(), userMiddleware, require('./routes/homeRouter'));
-router.use('/content', auth(), require('./routes/contentRouter'));
+router.use('/home', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/homeRouter'));
+router.use('/content', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/contentRouter'));
 router.use('/user', auth(), userMiddleware, require('./routes/userRouter'));
-router.use('/files', auth(), userMiddleware, require('./routes/fileRouter'));
-router.use('/suggestions', require('./routes/suggestionRouter'));
-router.use('/topic', auth(), userMiddleware, require('./routes/tagsRouter'));
-router.use('/author', auth(), userMiddleware, require('./routes/authorRouter'));
-router.use('/faq', auth(), require('./routes/faqRouter'));
-router.use('/house-rules', auth(), require('./routes/houseRulesRouter'));
-router.use('/myposts', auth(), userMiddleware, require('./routes/mypostsRouter'));
+router.use('/files', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/fileRouter'));
+router.use('/suggestions', userMiddleware, checkPseudonymMiddleware, require('./routes/suggestionRouter'));
+router.use('/topic', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/tagsRouter'));
+router.use('/author', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/authorRouter'));
+router.use('/faq', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/faqRouter'));
+router.use('/house-rules', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/houseRulesRouter'));
+router.use('/myposts', auth(), userMiddleware, checkPseudonymMiddleware, require('./routes/mypostsRouter'));
 
 module.exports = router;
