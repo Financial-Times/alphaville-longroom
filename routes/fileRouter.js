5cfa2bfed688f940377cb022baa7488d9f4c346c
Roland Vachter <roland.vachter@iquestgroup.com>
2017-01-16 10:19:45 +0200

    Do not require auth for file redirect route.

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const router = new express.Router();
 const fileCtrl = require('../lib/controllers/file');
+const auth = require('alphaville-auth-middleware');
 
-router.get('/sign', fileCtrl.sign);
-router.post('/delete', fileCtrl.delete);
+router.get('/sign', auth(), fileCtrl.sign);
+router.post('/delete', auth(), fileCtrl.delete);
 router.route('/attachment/:filePath').get(fileCtrl.oldAttachments);
 router.route('/:fileName').get(fileCtrl.download);
 
