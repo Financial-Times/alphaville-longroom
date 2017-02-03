f450787270ccd149bce2b07c7d6418c40ad0892a
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-02-03 11:35:28 +0200

    Update admin router

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const isEditor = require('../lib/middlewares/isEditor');
 const adminCtrl = require('../lib/controllers/admin');
 
-router.use('/', isEditor);
+router.all('/', isEditor);
 router.get('/', adminCtrl.index);
 router.get('/approve/:user_id', adminCtrl.approve);
 router.get('/reject/:user_id', adminCtrl.reject);
