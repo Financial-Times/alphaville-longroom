167f0740ebcc526cb41c4b6e9be8a9349054f9d0
Roland Vachter <roland.vachter@iquestgroup.com>
2017-02-09 16:01:32 +0200

    Fix pseudonym on dashboard and author pages.

--- router.js
+++ router.js
@@ -14,10 +14,10 @@ router.use('/user', auth(), require('./routes/userRouter'));
 router.use('/files', checkPseudonymMiddleware, require('./routes/fileRouter'));
 router.use('/suggestions', checkPseudonymMiddleware, require('./routes/suggestionRouter'));
 router.use('/topic', auth(), checkPseudonymMiddleware, require('./routes/tagsRouter'));
-router.use('/author', auth(), checkPseudonymMiddleware, fetchPseudonymMiddleware, require('./routes/authorRouter'));
+router.use('/author', auth(), checkPseudonymMiddleware, require('./routes/authorRouter'));
 router.use('/faq', auth(), checkPseudonymMiddleware, require('./routes/faqRouter'));
 router.use('/house-rules', auth(), checkPseudonymMiddleware, require('./routes/houseRulesRouter'));
-router.use('/dashboard', auth(), checkPseudonymMiddleware, require('./routes/mypostsRouter'));
+router.use('/dashboard', auth(), checkPseudonymMiddleware, fetchPseudonymMiddleware, require('./routes/mypostsRouter'));
 router.use('/search', auth(), checkPseudonymMiddleware, require('./routes/searchRouter'));
 router.use('/admin', auth(), require('./routes/adminRouter'));
 
