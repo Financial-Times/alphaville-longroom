eb3b0f82fadafb3eb9c0051dbb70e3e91fe4d45d
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-06-05 18:30:55 +0300

    (WIP) Update origami modules

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,20 @@ const env = process.env.ENVIRONMENT || 'test';
 
 alphavilleBuildTools(gulp, {
 	env: env,
-	buildFolder: 'public/build',
+	'build-folder': 'public/build',
 	builds: [
 		{
 			id: 'main',
 			standalone: 'mainBundle',
 			js: './assets/js/main.js',
 			sass: './assets/scss/main.scss',
-			buildJs: 'main.js',
-			buildCss: 'main.css'
+			'build-js': 'main.js',
+			'build-css': 'main.css'
 		},
 		{
 			id: 'wysiwyg',
 			sass: './assets/scss/tinymce_wysiwyg.scss',
-			buildCss: 'tinymce_wysiwyg.css'
+			'build-css': 'tinymce_wysiwyg.css'
 		}
 	]
 });
