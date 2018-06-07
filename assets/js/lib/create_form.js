12cfc876c5f5634a26aa0725b2640014ca139bae
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-06-07 18:17:38 +0300

    Replace o-overlay with o-toolip for tooltips

--- assets/js/lib/create_form.js
+++ assets/js/lib/create_form.js
@@ -1,9 +1,18 @@
 const LongroomCreateForm = require('./LongroomCreateForm');
+const Tooltip = require('o-tooltip');
 
 
 
 document.addEventListener('o.DOMContentLoaded', () => {
 	const forms = document.querySelectorAll('.longroom-create-post');
+	const tooltips = document.querySelectorAll('.lr-tooltip');
+
+	if (tooltips && tooltips.length) {
+		tooltips.forEach(t => {
+			new Tooltip(t);
+		});
+	}
+
 	if (forms && forms.length) {
 		for (let i = 0; i < forms.length; i++) {
 			const form = forms[i];
