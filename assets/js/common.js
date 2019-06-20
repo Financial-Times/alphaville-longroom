aab0c28e421ef291ed6ff6f45ae0b9f6ab3c3f5a
Tunca Bergmen <5130615+tbergmen@users.noreply.github.com>
2019-06-20 11:17:56 +0100

    Remove o-comments-beta from Longroom
    Until we find a more reliable way to test this...

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,7 @@
 require('./assets');
 require('alphaville-ui');
 require('o-expander');
-if (window.commentsTalkReplacement || window.commentsUseCoralTalk) {
-	// Temporary addition until the comments are replaced
-	require('o-comments-beta');
-} else {
-	require('o-comments');
-}
+require('o-comments');
 require('o-comment-count');
 require('./lib/deleteButton');
 require('./lib/ads');
