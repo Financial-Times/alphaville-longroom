7bbffb32a2ca4ec4922f016476f8de6d376ac1c1
Keran Braich <keran.braich@ft.com>
2019-11-13 10:24:09 +0000

    Remove Livefyre comments

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,7 @@
 require('./assets');
 require('alphaville-ui');
 require('o-expander');
-// Temporary addition until the comments are replaced
-if (window.commentsUseCoralTalk) {
-	require('o-comments-beta');
-} else {
-	require('o-comments');
-}
+require('o-comments');
 require('./lib/deleteButton');
 require('./lib/permutive');
 require('./lib/ads');
