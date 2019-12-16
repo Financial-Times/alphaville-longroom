cad7cf3c9d3d3a4247f1e2c33f121dcdb4a32e0b
Glynn Phillips <glynndominicphillips@gmail.com>
2019-12-16 14:54:58 +0000

    Adds an endpoint for syncing display names
    This endpoint will allow display name changes to be synced with
    Longroom.
    
    This will happen if the user signs up for Longroom and doesn't already
    have a display name so creates it during the sign up process. If they
    then go away and do create a commenting account and choose a different
    display name it will sync across to Longroom.
    
    They helps us manage the delay / race condition of users requesting
    access to Longroom and being accepted.

--- lib/sql/displayNames/updateDisplayName.sql
+++ lib/sql/displayNames/updateDisplayName.sql
@@ -1,5 +1,4 @@
-UPDATE display_names SET
-	display_name = ${display_name}
-WHERE
-	user_id = ${user_id}
-
+UPDATE display_names
+SET display_name = ${display_name}
+WHERE user_id = ${user_id}
+RETURNING user_id, display_name
