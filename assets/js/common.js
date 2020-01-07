e81b03a3af3d7090a1af632c45458db90b6f14f7
Glynn Phillips <glynndominicphillips@gmail.com>
2020-01-07 16:52:25 +0000

    Remove the old version of o-comments
    The old version was used to load livefyre comments, as we are no longer
    loading comments from livefyre we can promote the -beta version to be
    the stable version used.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,6 @@
 require('./assets');
 require('alphaville-ui');
-// Temporary addition until the comments are replaced
-if (window.commentsUseCoralTalk) {
-	require('o-comments-beta');
-} else {
-	require('o-comments');
-}
+require('o-comments');
 require('./lib/deleteButton');
 require('./lib/permutive');
 require('./lib/ads');
