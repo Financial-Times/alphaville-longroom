c35e0bbf51853cd2c53433408de94b40237c2635
alevito <alessandro.vito84@gmail.com>
2020-04-06 18:07:27 +0100

    ADSDEV-449 Use n-permutive for Alphaville

--- assets/js/lib/permutive.js
+++ assets/js/lib/permutive.js
@@ -1,6 +1,22 @@
-const contentId = document.documentElement.dataset.contentId;
-const Permutive = require('alphaville-ui')['permutive'];
+const nPermutive = require('n-permutive');
 
-Permutive.initPermutive();
-Permutive.setUserAndContent(contentId);
-Permutive.setPermutiveSegments();
+const PERMUTIVE_CREDENTIALS = {
+	publicId: 'e1c3fd73-dd41-4abd-b80b-4278d52bf7aa',
+	publicApikey: 'b2b3b748-e1f6-4bd5-b2f2-26debc8075a3',
+}
+
+const getAppName = targeting => targeting && targeting.auuid
+	? 'article'
+	: 'homepage';
+
+nPermutive.setup(PERMUTIVE_CREDENTIALS);
+nPermutive.loadPermutiveScript(PERMUTIVE_CREDENTIALS.publicId);
+
+// Wait for oAds to complete initialisation, in order to access the targeting meta
+// and Then identify the user and track PageView
+oAds.utils.on('initialised', () => {
+	const targeting = oAds.targeting.get();
+
+	nPermutive.identifyUser(targeting);
+	nPermutive.trackPageView(targeting, getAppName(targeting), oAds.config('behavioralMeta'));
+})
