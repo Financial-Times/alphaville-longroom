60cc4353c2a48b81e7f32212e67762883444d9d1
alevito <alessandro.vito84@gmail.com>
2020-10-26 13:53:31 +0000

    ADSDEV-705 replace n-permutive with @financial-times/ads-permutive

--- assets/js/lib/permutive.js
+++ assets/js/lib/permutive.js
@@ -1,21 +1,21 @@
-const nPermutive = require('n-permutive').default;
+const adsPermutive = require('@financial-times/ads-permutive').default;
 
 const getPageViewType = pageView => pageView && pageView.article
 	? 'article'
 	: 'homepage'
 
-nPermutive.setup();
-nPermutive.loadPermutiveScript();
+adsPermutive.setup();
+adsPermutive.loadPermutiveScript();
 
 // Wait for oAds to complete initialisation, in order to access the targeting meta
 // and Then identify the user and track PageView
 document.body.addEventListener('oAds.initialised', () => {
 	const [user, article] = oAds.api.data;
-	const pageView = nPermutive.fromAdsApiV1ToPageView({ user, article });
+	const pageView = adsPermutive.fromAdsApiV1ToPageView({ user, article });
 
-	nPermutive.identifyUser({
+	adsPermutive.identifyUser({
 		uuid: user && user.uuid
 	});
 	pageView && pageView.page && (pageView.page.type = getPageViewType());
-	nPermutive.trackPageView(pageView);
+	adsPermutive.trackPageView(pageView);
 })
