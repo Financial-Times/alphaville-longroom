d7209e65c87f64f0d9d1cb91b8a1441cf8380f02
AliceArmstrong <22509772+AliceArmstrong@users.noreply.github.com>
2022-01-20 15:26:19 +0000

    Install npm versions of origami packages

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 require('./lib/permutive');
 require('./assets');
 require('alphaville-ui');
-require('o-comments');
+require('@financial-times/o-comments');
 require('./lib/deleteButton');
 require('./lib/ads');
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,4 @@ require('./lib/create_form');
 require('./lib/join_form');
 require('./lib/faq');
 
-require('o-autoinit');
+require('@financial-times/o-autoinit');
