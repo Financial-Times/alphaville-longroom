baa07e820fb01ae8afdf1ef33b8874180c967483
Jake Champion <me@jakechampion.name>
2022-02-04 12:42:33 +0000

    use node-sass directly instead of via obt and install origami via peer-dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,7 @@ const exec = require("child_process").exec;
 const fork = require("child_process").fork;
 const fs = require("fs");
 const path = require("path");
-
-const env = process.env.ENVIRONMENT || 'test';
+const sass = require('gulp-sass')(require('sass'));
 
 function ensureDirectoryExistence(filePath) {
 	const dirname = path.dirname(filePath);
@@ -40,143 +39,143 @@ function generateRandomString(length) {
 	return text;
 }
 
-const alphavilleBuildTools = function (gulp, config) {
-	const buildFolder = config["build-folder"] || "public/build";
-	const env = config.env === "prod" ? "prod" : "test";
+const buildFolder = "public/build";
 
-	gulp.task("build-config-dir", function (done) {
-		ensureDirectoryExistence("build_config/js/test.js");
-		ensureDirectoryExistence("build_config/scss/test.scss");
-		done();
-	});
+gulp.task("build-config-dir", function (done) {
+	ensureDirectoryExistence("build_config/js/test.js");
+	ensureDirectoryExistence("build_config/scss/test.scss");
+	done();
+});
 
-	gulp.task("fingerprint", function (callback) {
-		const fingerprint = generateRandomString(10);
+gulp.task("fingerprint", function (callback) {
+	const fingerprint = generateRandomString(10);
 
-		fs.writeFileSync(
-			"build_config/js/fingerprint.js",
-			`module.exports="${fingerprint}";`
-		);
-		fs.writeFileSync(
-			"build_config/scss/fingerprint.scss",
-			`$fingerprint: '${fingerprint}';`
-		);
-		callback();
-	});
+	fs.writeFileSync(
+		"build_config/js/fingerprint.js",
+		`module.exports="${fingerprint}";`
+	);
+	fs.writeFileSync(
+		"build_config/scss/fingerprint.scss",
+		`$fingerprint: '${fingerprint}';`
+	);
+	callback();
+});
 
-	gulp.task("assets-domain-config", function (callback) {
-		const assetsDomain = "";
+gulp.task("assets-domain-config", function (callback) {
+	const assetsDomain = "";
 
-		fs.writeFileSync(
-			"build_config/js/assetsDomain.js",
-			`module.exports="${assetsDomain}";`
-		);
-		fs.writeFileSync(
-			"build_config/scss/assetsDomain.scss",
-			`$assets-domain: '${assetsDomain}';`
-		);
-
-		callback();
-	});
-
-	gulp.task("bower-install", function (callback) {
-		exec("./node_modules/bower/bin/bower install", callback);
-	});
-
-	gulp.task("bower-cache-clean", function (callback) {
-		exec("./node_modules/bower/bin/bower cache clean", callback);
-	});
-
-	gulp.task("bower-folder-clean", function (callback) {
-		del(["./bower_components"], callback);
-	});
-
-	gulp.task(
-		"bower-clean",
-		gulp.series("bower-cache-clean", "bower-folder-clean")
+	fs.writeFileSync(
+		"build_config/js/assetsDomain.js",
+		`module.exports="${assetsDomain}";`
+	);
+	fs.writeFileSync(
+		"build_config/scss/assetsDomain.scss",
+		`$assets-domain: '${assetsDomain}';`
 	);
 
-	gulp.task("clean-build", function (callback) {
-		del([buildFolder], callback);
-	});
+	callback();
+});
 
-	gulp.task("obt-verify", function (callback) {
-		exec("./node_modules/.bin/obt verify", callback);
-	});
+gulp.task("bower-install", function (callback) {
+	exec("./node_modules/bower/bin/bower install", callback);
+});
 
-	const buildTasks = [];
-	config.builds.forEach(function (buildConfig) {
-		const baseBuildConfig = {};
+gulp.task("bower-cache-clean", function (callback) {
+	exec("./node_modules/bower/bin/bower cache clean", callback);
+});
 
-		baseBuildConfig["build-folder"] = buildFolder;
+gulp.task("bower-folder-clean", function (callback) {
+	del(["./bower_components"], callback);
+});
 
-		if (env) {
-			baseBuildConfig.env = env === "prod" ? "production" : "development";
-		}
+gulp.task(
+	"bower-clean",
+	gulp.series("bower-cache-clean", "bower-folder-clean")
+);
 
-		Object.assign(baseBuildConfig, buildConfig);
+gulp.task("clean-build", function (callback) {
+	del([buildFolder], callback);
+});
 
-		const buildTaskName =
-			"obt-build-" + (buildConfig.id || generateRandomString(5));
-		buildTasks.push(buildTaskName);
+gulp.task("obt-verify", function (callback) {
+	exec("./node_modules/.bin/obt verify", callback);
+});
 
-		gulp.task(buildTaskName, function (callback) {
-			let command = "./node_modules/.bin/obt";
-			let args = ["build"];
+const buildTasks = [];
+[
+	{
+		id: 'main',
+		standalone: 'mainBundle',
+		js: './assets/js/main.js',
+		sass: './assets/scss/main.scss',
+		'build-js': 'main.js',
+		'build-css': 'main.css'
+	},
+	{
+		id: 'wysiwyg',
+		sass: './assets/scss/tinymce_wysiwyg.scss',
+		'build-css': 'tinymce_wysiwyg.css'
+	}
+].forEach(function (buildConfig) {
+	const baseBuildConfig = {};
 
-			Object.keys(baseBuildConfig).forEach((key) => {
-				args.push(`--${key}`, baseBuildConfig[key]);
-			});
+	baseBuildConfig["build-folder"] = buildFolder;
+
+	baseBuildConfig.env = process.env.ENVIRONMENT === "prod" ? "production" : "development";
+
+	Object.assign(baseBuildConfig, buildConfig);
+
+	const buildTaskName = "obt-build-" + (buildConfig.id || generateRandomString(5));
+	buildTasks.push(buildTaskName);
+
+	gulp.task(buildTaskName, function (callback) {
+		if (baseBuildConfig.sass) {
+			let command = "./node_modules/.bin/sass";
+			let args = ["-I", "bower_components/", "-I", "node_modules/", "-I", "node_modules/@financial-times/", baseBuildConfig.sass, path.join(buildFolder, baseBuildConfig['build-css'])];
 
 			const runCmd = fork(command, args);
 			runCmd.on("error", (err) => console.log(err));
 			runCmd.on("close", () => {
 				callback();
 			});
+		}
+		delete baseBuildConfig.sass;
+		delete baseBuildConfig['build-css'];
+
+
+		let command = "./node_modules/.bin/obt";
+		let args = ["build"];
+
+		Object.keys(baseBuildConfig).forEach((key) => {
+			args.push(`--${key}`, baseBuildConfig[key]);
+		});
+
+		const runCmd = fork(command, args);
+		runCmd.on("error", (err) => console.log(err));
+		runCmd.on("close", () => {
+			callback();
 		});
 	});
+});
 
-	gulp.task("obt-build", gulp.series(...buildTasks));
+gulp.task("obt-build", gulp.series(...buildTasks));
 
-	gulp.task("verify", gulp.series("obt-verify"));
-	gulp.task(
-		"build",
-		gulp.series(
-			"clean-build",
-			"build-config-dir",
-			"fingerprint",
-			"assets-domain-config",
-			"obt-build"
-		)
-	);
+gulp.task("verify", gulp.series("obt-verify"));
+gulp.task(
+	"build",
+	gulp.series(
+		"clean-build",
+		"build-config-dir",
+		"fingerprint",
+		"assets-domain-config",
+		"obt-build"
+	)
+);
 
-	gulp.task("obt", gulp.series("verify", "build"));
+gulp.task("obt", gulp.series("verify", "build"));
 
-	gulp.task("default", gulp.series("bower-clean", "bower-install", "build"));
+gulp.task("default", gulp.series("bower-clean", "bower-install", "build"));
 
-	gulp.task("watch", function () {
-		gulp.watch(["./assets/**"], ["obt-build"]);
-	});
-
-	return gulp;
-};
-
-alphavilleBuildTools(gulp, {
-	env: env,
-	'build-folder': 'public/build',
-	builds: [
-		{
-			id: 'main',
-			standalone: 'mainBundle',
-			js: './assets/js/main.js',
-			sass: './assets/scss/main.scss',
-			'build-js': 'main.js',
-			'build-css': 'main.css'
-		},
-		{
-			id: 'wysiwyg',
-			sass: './assets/scss/tinymce_wysiwyg.scss',
-			'build-css': 'tinymce_wysiwyg.css'
-		}
-	]
+gulp.task("watch", function () {
+	gulp.watch(["./assets/**"], ["obt-build"]);
 });
