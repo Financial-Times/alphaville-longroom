b32532f81e01572485b328b7cd01fd88af7932b1
Jake Champion <me@jakechampion.name>
2022-02-15 14:25:49 +0000

    add comment about why permutive is commented out

--- assets/js/lib/permutive.js
+++ assets/js/lib/permutive.js
@@ -10,6 +10,11 @@ adsPermutive.loadPermutiveScript();
 // Wait for oAds to complete initialisation, in order to access the targeting meta
 // and Then identify the user and track PageView
 document.body.addEventListener('oAds.initialised', () => {
+	// TODO - We need to figure out how to migrate to the new permutive library
+	// we are using version 0.3.0
+	// The latest version is 4.0.0 - we can't upgrade to v4 without changing the build-pipeline
+	//
+
 	// const [user, article] = oAds.api.data;
 	// const pageView = adsPermutive.fromAdsApiV1ToPageView({ user, article });
 
